848b1577f5cfdcfc6c1a36fdae250344a049b3cf
kalema1 <kalemar8@gmail.com>
2024-08-28 15:05:39 +0300

    create a HomePage component
    - installed reacr-router-dom for page navigation.
    - installed vite-plugins-eslient for lint the code.
    - created the HomePage component for the Home page.
    - created the Logo component for the application Logo.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Homepage";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
