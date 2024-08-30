1e10a1a8c8874980f621e9da6292efbd3e332b67
kalema1 <kalemar8@gmail.com>
2024-08-30 13:18:21 +0300

    create a ContantUs component
    - created the ContactUs component.
    - added the component to the pages route.
    - added the Menu component to the pages route.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage";
+import Menu from "./pages/Menu";
+import ContactUs from "./pages/ContactUs";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="menu" element={<Menu />} />
+        <Route path="contact-us" element={<ContactUs />} />
       </Routes>
     </BrowserRouter>
   );
