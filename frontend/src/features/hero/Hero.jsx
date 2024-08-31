90d561ae24f14cde67b8602cc41664b2262064c1
kalema1 <kalemar8@gmail.com>
2024-08-31 14:30:55 +0300

    create a hero section
    - created the Hero component.
    - added content in the Hero section.
    - added styles in the Hero section.

---
+++ frontend/src/features/hero/Hero.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+  return (
+    <section className={styles.heroSection}>
+      <div className="container">
+        <div className={styles.heroContainer}>
+          <h1 className={styles.heroHeading}>Order your favourite food here</h1>
+          <p className={styles.heroPara}>
+            Choose from a diverse menu featuring a delectable array of dishes
+            crafted with the finest ingredients and culinary expertise. Our
+            mission is to satisfy your cravings and elevate your dinning
+            experience, one delicious meal at a time.
+          </p>
+          <Link>View Menu</Link>
+        </div>
+      </div>
+    </section>
+  );
+}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
+import Hero from "../features/hero/Hero";
 import PageNav from "../features/pageNavigation/PageNav";
 
 export default function HomePage() {
   return (
     <div>
       <PageNav />
+      <Hero />
     </div>
   );
 }
