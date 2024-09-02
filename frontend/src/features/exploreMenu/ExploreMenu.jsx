2a0bdd735f543a5330b24e5caaaae2613028c75f
kalema1 <kalemar8@gmail.com>
2024-09-02 11:51:13 +0300

    add content to the ExploreMenu component
    - added the elements to the ExploreMenu component.

--- frontend/src/features/exploreMenu/ExploreMenu.jsx
+++ frontend/src/features/exploreMenu/ExploreMenu.jsx
@@ -1,3 +1,15 @@
 export default function ExploreMenu() {
-  return <div>Menu List</div>;
+  return (
+    <section>
+      <div className="container">
+        <div className={StyleSheet.ExploreMenuContainer}>
+          <h2>Explore Our Menu</h2>
+          <p>
+            Choose from a diverse menu featuring a delectable array of dishes
+          </p>
+          <div></div>
+        </div>
+      </div>
+    </section>
+  );
 }
