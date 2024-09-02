3e112040b5f7fb5416298306c6948f63402b0d47
kalema1 <kalemar8@gmail.com>
2024-09-02 15:15:13 +0300

    add media query styles
    - added media query for differnt screen sizes. To make the application
      responsive and look good.

--- frontend/src/data/menuList.js
+++ frontend/src/data/menuList.js
@@ -4,7 +4,7 @@ export const menuList = [
   { menuName: "Deserts", menuImage: "frontend_assets/menu_3.png" },
   { menuName: "Sandwish", menuImage: "frontend_assets/menu_4.png" },
   { menuName: "Cake", menuImage: "frontend_assets/menu_5.png" },
-  { menuName: "Pure Veg", menuImage: "frontend_assets/menu_6.png" },
+  { menuName: "Veg", menuImage: "frontend_assets/menu_6.png" },
   { menuName: "Pasta", menuImage: "frontend_assets/menu_7.png" },
   { menuName: "Noddles", menuImage: "frontend_assets/menu_8.png" },
 ];
