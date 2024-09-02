dbef5404cb088d8e643ba52e1c921c30d88436a7
kalema1 <kalemar8@gmail.com>
2024-09-02 11:40:33 +0300

    create menuList file
    - created menuList.js file. To contain the list of menu.
    - added data to the menuList.js file.

---
+++ frontend/src/data/menuList.js
@@ -0,0 +1,10 @@
+export const menuList = [
+  { menuName: "Salad", MenuImage: "frontend_assets/menu_1.png" },
+  { menuName: "Rolls", MenuImage: "frontend_assets/menu_2.png" },
+  { menuName: "Deserts", MenuImage: "frontend_assets/menu_3.png" },
+  { menuName: "Sandwish", MenuImage: "frontend_assets/menu_4.png" },
+  { menuName: "Cake", MenuImage: "frontend_assets/menu_5.png" },
+  { menuName: "Pure Veg", MenuImage: "frontend_assets/menu_6.png" },
+  { menuName: "Pasta", MenuImage: "frontend_assets/menu_7.png" },
+  { menuName: "Noddles", MenuImage: "frontend_assets/menu_8.png" },
+];
