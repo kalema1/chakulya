395e3c689164e92468bfe4f9d473eadc4f661559
kalema1 <kalemar8@gmail.com>
2024-09-02 14:26:23 +0300

    add styles for the ExploreMenu component
    - added styles for the ExploreMenu component. To make the element
      responsive and look good on all evice screens.

--- frontend/src/features/exploreMenu/ExploreMenu.jsx
+++ frontend/src/features/exploreMenu/ExploreMenu.jsx
@@ -1,15 +1,17 @@
 import { menuList } from "../../data/menuList";
 import ExploreMenuListItem from "./ExploreMenuListItem";
+import styles from "./ExploreMenu.module.css";
+
 export default function ExploreMenu() {
   return (
-    <section>
+    <section className={styles.section}>
       <div className="container">
         <div className={StyleSheet.ExploreMenuContainer}>
-          <h2>Explore Our Menu</h2>
-          <p>
+          <h2 className={styles.subHeading}>Explore Our Menu</h2>
+          <p className={styles.para}>
             Choose from a diverse menu featuring a delectable array of dishes
           </p>
-          <div>
+          <div className={styles.menuList}>
             {menuList.map((menuItem, index) => (
               <ExploreMenuListItem menuItem={menuItem} key={index} />
             ))}
