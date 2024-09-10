86a970052d7bbe5aee6cd3af2920a21fba6b1154
kalema1 <kalemar8@gmail.com>
2024-09-10 20:22:03 +0300

    add styles to the NavBarRight component
    - added more styles to the component.

--- frontend/src/features/pageNavigation/NavBarRight.jsx
+++ frontend/src/features/pageNavigation/NavBarRight.jsx
@@ -4,11 +4,19 @@ import SignIn from "./SignIn";
 export default function NavBarRight() {
   return (
     <div className={styles.navBarRightContainer}>
-      <div>
-        <img src="frontend_assets/search_icon.png" alt="" />
+      <div className={styles.searchBox}>
+        <img
+          className={styles.image}
+          src="frontend_assets/search_icon.png"
+          alt=""
+        />
       </div>
       <div className={styles.cartBox}>
-        <img src="frontend_assets/basket_icon.png" alt="" />
+        <img
+          className={styles.image}
+          src="frontend_assets/basket_icon.png"
+          alt=""
+        />
         <div className={styles.cartItems}>5</div>
       </div>
       <div className={styles.signInContainer}>
