a28c139ad3c4dc1efd263ae4eedfabebffff7a98
kalema1 <kalemar8@gmail.com>
2024-09-12 00:46:03 +0300

    add logic to CartContext
    - added logic to the CartContext.
    - remove the logic from the useFoodItem custom hook.

--- frontend/src/contexts/CartContext.jsx
+++ frontend/src/contexts/CartContext.jsx
@@ -1,7 +1,25 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const CartContext = createContext();
 
-export default function CartProvider({children}) {
-  return <CartContext.Provider value={}>{children}</CartContext.Provider>;
+export default function CartProvider({ children }) {
+  const [addToCart, setAddToCart] = useState(0);
+
+  function addItemsToCart() {
+    setAddToCart((add) => add + 1);
+  }
+
+  function removeItemsFromCart() {
+    setAddToCart((remove) => remove - 1);
+  }
+
+  return (
+    <CartContext.Provider
+      value={{ addItemsToCart, removeItemsFromCart, addToCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
+
+export { CartProvider };
--- frontend/src/features/food/useFoodItem.js
+++ frontend/src/features/food/useFoodItem.js
@@ -1,15 +1,3 @@
-import { useState } from "react";
-
 export function useFoodItem() {
-  const [addToCart, setAddToCart] = useState(0);
-
-  function addItemsToCart() {
-    setAddToCart((add) => add + 1);
-  }
-
-  function removeItemsFromCart() {
-    setAddToCart((remove) => remove - 1);
-  }
-
   return { addToCart, addItemsToCart, removeItemsFromCart };
 }
