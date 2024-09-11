caee9524202caeb2b36e63bd6918ed003c1d19c4
kalema1 <kalemar8@gmail.com>
2024-09-12 00:51:03 +0300

    provide the CartContext to the application
    - provided the CartContext to the entire application. To broadcast
      state.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import Menu from "./pages/Menu";
 import ContactUs from "./pages/ContactUs";
+import CartProvider from "./contexts/CartContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="menu" element={<Menu />} />
-        <Route path="contact-us" element={<ContactUs />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="menu" element={<Menu />} />
+          <Route path="contact-us" element={<ContactUs />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
